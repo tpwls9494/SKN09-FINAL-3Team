a2e623e9a6c4f12e9e3f07e48ca0058f445635e0
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2025-06-05 11:29:30 +0900

    back end migration

--- pass_project/pass_project/settings.py
+++ pass_project/pass_project/settings.py
@@ -136,3 +136,5 @@ CSRF_COOKIE_HTTPONLY = False
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
