7e265ea33bc5b7f9023f7d1c1dfd96c964d1ac0a
Hack012 <veetoveen@naver.com>
2025-06-05 17:40:08 +0900

    2025-06-05 전성원 Login 기능 구현

--- pass_project/accounts/urls.py
+++ pass_project/accounts/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('ajax-login/', views.ajax_login, name='ajax_login'),
+    path('ajax-insert-login-log/', views.ajax_insert_login_log, name='ajax_insert_login_log'),
+    path('get-prev-login-user/', views.get_prev_login_user, name='get_prev_login_user'),
     path('mypage/', views.mypage_view, name='mypage'),
     path('mypage/repassword/', views.repassword_view, name='repassword'),
 ]
